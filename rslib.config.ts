e24adebedddad2f3443db217fa1ed151ef716720
vekonylaszlo-biotechusa <vekony.laszlo@biotechusa.com>
2026-06-16 10:08:58 +0200

    trying to fix export types

--- rslib.config.ts
+++ rslib.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "@rslib/core";
 export default defineConfig({
   source: {
     entry: {
-      index: ["./src/**"],
+      index: "./src/index.ts",
     },
   },
   resolve: {
